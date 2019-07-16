0a819217d88135f3cf987be1d7f2b24987b3bd8e
Andreas Stassivik <andreas@stassi.net>
2019-07-16 16:16:11 -0700

    extraction

--- src/stringDistance/levenshtein/levenshtein.js
+++ src/stringDistance/levenshtein/levenshtein.js
@@ -1,11 +1,8 @@
-import { dec, inc } from 'ramda';
 import mapDecrement from './mapDecrement';
 import mapLength from './mapLength';
 import debugPredicateAlpha from './debugPredicateAlpha';
 import debugTransformBeta from './debugTransformBeta';
-
-const CODES = [];
-const VECTOR = [];
+import debugTransformDelta from './debugTransformDelta';
 
 const levenshtein = (alpha, beta) => {
   let [
@@ -45,61 +42,15 @@ const levenshtein = (alpha, beta) => {
     betaLength,
   ]);
 
-  if (!alphaLengthFinal) {
-    // console.log('No alphaLengthFinal, returning betaLengthFinal');
-    return betaLengthFinal;
-  }
-
-  const v0 = VECTOR;
-
-  let i = 0;
-
-  while (i < betaLengthFinal) {
-    CODES[i] = beta.charCodeAt(startWithoutPrefix + i);
-    i = inc(i);
-    v0[dec(i)] = i;
-  }
-
-  let current = 0;
-  let left;
-  let above;
-  let charA;
-  let j;
-
-  // eslint-disable-next-line no-plusplus
-  for (i = 0; i < alphaLengthFinal; i++) {
-    // console.log('Starting the nested loops');
-    left = i;
-    current = i + 1;
-
-    charA = alpha.charCodeAt(startWithoutPrefix + i);
-
-    // eslint-disable-next-line no-plusplus
-    for (j = 0; j < betaLengthFinal; j++) {
-      above = current;
-
-      current = left;
-      left = v0[j];
-
-      if (charA !== CODES[j]) {
-        // Insertion
-        if (left < current) {
-          current = left;
-        }
-
-        // Deletion
-        if (above < current) {
-          current = above;
-        }
-
-        current = inc(current);
-      }
-
-      v0[j] = current;
-    }
-  }
-
-  return current;
+  return alphaLengthFinal
+    ? debugTransformDelta({
+      alpha,
+      alphaLengthFinal,
+      beta,
+      betaLengthFinal,
+      startWithoutPrefix,
+    })
+    : betaLengthFinal;
 };
 
 export default levenshtein;
