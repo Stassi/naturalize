b1f95d835a459af90b7f7f9b176c5931052dfca7
Andreas Stassivik <andreas@stassi.net>
2019-07-17 21:14:59 -0700

    composition

--- src/stringDistance/levenshtein/debugPredicateAlpha.js
+++ src/stringDistance/levenshtein/debugPredicateAlpha.js
@@ -1,19 +1,17 @@
 import applyAnd from './applyAnd';
 import applyEquals from './applyEquals';
-import greaterThanZero from './greaterThanZero';
-import negateBitwiseNot from './negateBitwiseNot';
+import headGreaterThanZero from './headGreaterThanZero';
+import mapNegateBitwiseNot from './mapNegateBitwiseNot';
 
-const debugPredicateAlpha = ([alpha, beta]) => ([alphaLength, betaLength]) => {
-  const [alphaUTF16, betaUTF16] = [
-    negateBitwiseNot(alphaLength),
-    negateBitwiseNot(betaLength),
-  ];
+const debugPredicateAlpha = strings => (lengths) => {
+  const [alpha, beta] = strings;
+  const alphaLengthGreaterThanZero = headGreaterThanZero(lengths);
+  const [alphaUTF16, betaUTF16] = mapNegateBitwiseNot(lengths);
   const predicateDelta = [
     alpha.charCodeAt(alphaUTF16),
     beta.charCodeAt(betaUTF16),
   ];
   const debugPredicateAlphaBeta = applyEquals(predicateDelta);
-  const alphaLengthGreaterThanZero = greaterThanZero(alphaLength);
   const predicates = [alphaLengthGreaterThanZero, debugPredicateAlphaBeta];
   return applyAnd(predicates);
 };
