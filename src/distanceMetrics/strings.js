c617668729a3226e7ff955d173231a5f6c1e363c
Andreas Stassivik <andreas@stassi.net>
2019-07-24 15:35:30 -0700

    jaroWinkler (+distance, -similarity)

--- src/distanceMetrics/strings.js
+++ src/distanceMetrics/strings.js
@@ -6,7 +6,7 @@ import hamming from 'talisman/metrics/distance/hamming';
 import { distance as identity } from 'talisman/metrics/distance/identity';
 import { distance as jaccard } from 'talisman/metrics/distance/jaccard';
 import { distance as jaro } from 'talisman/metrics/distance/jaro';
-import jaroWinkler from 'talisman/metrics/distance/jaro-winkler';
+import { distance as jaroWinkler } from 'talisman/metrics/distance/jaro-winkler';
 import lcs from 'talisman/metrics/distance/lcs';
 import length from 'talisman/metrics/distance/length';
 import levenshtein from 'talisman/metrics/distance/levenshtein';
