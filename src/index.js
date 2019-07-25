1d664c89ee8ee9a3b76e44d60ae364f3bd43ea16
Andreas Stassivik <andreas@stassi.net>
2019-07-24 20:06:08 -0700

    +mlipnsDistance

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import {
   lig2 as lig2Distance,
   lig3 as lig3Distance,
   minHash as minHashDistance,
+  mlipns as mlipnsDistance,
   sorensenDice as sorensenDiceDistance,
 } from './distanceMetrics/strings';
 
@@ -33,5 +34,6 @@ export {
   lig2Distance,
   lig3Distance,
   minHashDistance,
+  mlipnsDistance,
   sorensenDiceDistance,
 };
