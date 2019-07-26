fbbf68725ae0c2900c570fa0da8bcc2730246e3d
Andreas Stassivik <andreas@stassi.net>
2019-07-26 05:21:28 -0700

    simplify

--- src/stringMetrics.js
+++ src/stringMetrics.js
@@ -1,16 +1,11 @@
-import {
-  identity,
-  pipe,
-  subtract,
-} from './utilities';
 import * as metrics from './distanceMetrics/strings';
+import {
+  subtractFromOne as toSimilarity,
+} from './distanceMetrics/subtractFromOne';
 
-const stringMetrics = ({
-  name,
-  similarity,
-}) => pipe(
-  metrics[name],
-  similarity ? subtract(1) : identity,
-);
+const stringMetrics = ({ name, similarity }) => {
+  const distance = metrics[name];
+  return similarity ? toSimilarity(distance) : distance;
+};
 
 export default stringMetrics;
