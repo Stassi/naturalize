af09d8c6dd7fff0d14555399fdba860f7f795c6e
Andreas Stassivik <andreas@stassi.net>
2019-08-01 17:28:28 -0700

    +mapItemIncludedIn

---
+++ src/mapItemIncludedIn.js
@@ -0,0 +1,7 @@
+import itemIncludedIn from './itemIncludedIn';
+import map from './map';
+
+// TODO: Pipe
+const mapItemIncludedIn = x => map(itemIncludedIn(x));
+
+export default mapItemIncludedIn;
